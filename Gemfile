a1c85fcd34ae8fe1289518e4e99a7a815c19147d
Yina Zhu <yinagit@gmail.com>
2018-06-06 03:23:06 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -4,8 +4,6 @@ gem 'activerecord', :require => 'active_record'
 gem 'sinatra-activerecord', :require => 'sinatra/activerecord'
 
 gem 'sinatra'
-gem 'sqlite3'
-gem 'tux'
 gem 'pry'
 gem 'pry-nav'
 gem 'rake'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,16 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
-
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+    @post = Post.create(params)
+    redirect to 'posts'
+  end
+
+  
+
+
 end
